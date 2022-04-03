4e27095ec679499d50ac91b47eb24b6fb80e9862
OliGarc333 <ngarcia333@gmail.com>
2022-04-03 21:45:20 +0200

    Iter. 1-3

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
+
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
@@ -20,6 +20,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer');
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
